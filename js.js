59e48f6653b0d1af6b25296df358b7edecd2670e
jp <jp@blood.localdomain>
2020-12-17 14:41:27 +1000

    better calc

--- js.js
+++ js.js
@@ -1,30 +1,48 @@
-function btnPress(keyVal) {
-    var numberToShow = outscreen.value;
-    numberToShow = (numberToShow * 10) + keyVal;
-    outscreen.value = numberToShow;
+var num1 = 0;
+var num2 = 0;
+var opNum = '';
+var makeDec = 0;
+
+function addNum(btnPress) {
+    if(makeDec == 0) {
+        num1 = num1 * 10 + btnPress;
+    } else {
+        num1 = num1 + (btnPress / makeDec);
+        makeDec = makeDec * 10;
+    }
+    document.getElementById('screen').innerHTML = num1;
+    console.log(num1);
+}
+function mkDec() {
+    if(makeDec == 0) {
+        makeDec = 10;
+    } else {
+        makeDec = makeDec * 10;
+    }
+}
+function operateNum(operator) {
+    opNum = operator;
+    num2 = num1;
+    num1 = 0;
+    makeDec = 0;
+}
+function calcNum() {
+    var outNum = 0;
+    if(opNum == '+') {
+        outNum = num2 + num1;
+    }
+    if(opNum == '-') {
+        outNum = num2 - num1;
+    }
+    if(opNum == '*') {
+        outNum = num2 * num1;
+    }
+    if(opNum == '/') {
+        outNum = num2 / num1;
+    }
+    document.getElementById('screen').innerHTML = outNum;
+    num1 = outNum;
+    num2 = 0;
+    makeDec = 0;
 }
 
-function doCalc() {
-    var newVal;
-    if(calctype.value == '+') {
-        newVal = Number(secondval.value) + Number(outscreen.value);
-    }
-    if(calctype.value == '-') {
-        newVal = secondval.value - outscreen.value;
-    }
-    if(calctype.value == '*') {
-        newVal = secondval.value * outscreen.value;
-    }
-    if(calctype.value == '/') {
-        newVal = secondval.value / outscreen.value;
-    }
-    secondval = 0;
-    calctype.value = 0;
-    outscreen.value = newVal;
-}
-
-function calcType(keyVal) {
-    calctype.value = keyVal;
-    secondval.value = outscreen.value;
-    outscreen.value = '';
-}
